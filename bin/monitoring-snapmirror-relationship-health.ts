e39822ea57df1408bfbaee8f796f18f3049dd97b
non-97 <nonmonaka07@gmail.com>
2024-12-12 17:59:44 +0900

    Stack名が命名規約に沿った形になるように修正

--- bin/monitoring-snapmirror-relationship-health.ts
+++ bin/monitoring-snapmirror-relationship-health.ts
@@ -4,11 +4,17 @@ import * as cdk from "aws-cdk-lib";
 import { MonitoringSnapMirrorRelationshipHealthStack } from "../lib/monitoring-snapmirror-relationship-health-stack";
 import { monitoringSnapMirrorRelationshipHealthStackProperty } from "../parameter/index";
 
+const stackName = monitoringSnapMirrorRelationshipHealthStackProperty.props
+  .systemProperty
+  ? `${monitoringSnapMirrorRelationshipHealthStackProperty.props.systemProperty.systemName}-${monitoringSnapMirrorRelationshipHealthStackProperty.props.systemProperty.envName}-stack-monitoring-snapmirror-health`
+  : "FsxnResourcesStack";
+
 const app = new cdk.App();
 const stack = new MonitoringSnapMirrorRelationshipHealthStack(
   app,
   "MonitoringSnapmirrorRelationshipHealthStack",
   {
+    stackName,
     env: monitoringSnapMirrorRelationshipHealthStackProperty.env,
     ...monitoringSnapMirrorRelationshipHealthStackProperty.props,
   }
